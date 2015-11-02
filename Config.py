4f92cbb5031bce20d63001fc99fa4ec519220d82
Scott Farley <scott@firesphere.org>
2015-11-02 15:14:48 -0600

    Removed API keys

--- Config.py
+++ Config.py
@@ -6,13 +6,13 @@ class Configuration():
         self.consumerKey = consumerKey
 
 
-Twitter = Configuration(apiKey="BM3NAGuWx4iMpaYBB29hoyJeH", apiSecret="lzsHayWBop09eNwuKL7fDLTevN1X6pEqQykicmij4XcToPdWwj",
-                         consumerKey="3609701009-mDbbwgQkNbvumolwMn43cOHBfNE4mun49lEdqeJ", consumerSecret="Khjzhlfp6pkbeB5urxbWfYfQCIX3BWOKFLMjm9cpQTTZ7")
+Twitter = Configuration(apiKey="", apiSecret="",
+                         consumerKey="", consumerSecret="")
 
 
-Guardian = Configuration(apiKey="h4sjfafaf56m7p29bw9qmqyx")
-NPR = Configuration(apiKey="MDIxMDEwNjc0MDE0NDU5NzYxODQ4MTVhZg000")
-Tumblr = Configuration(apiKey="pKHOq4GdpCnJ2QdGh5jVgah7EmZ7vqYTc9k5bPgVxQpTOgblK2", apiSecret="4gqIuDfozMxW5ol4fmy7Zk9xBoFcRevoZ1gB1GIEcfQCSG94hU")
-NYT = Configuration(apiKey="389fb66286e98e35e0a6f7d8d8d9abb0:18:73270480")
+Guardian = Configuration(apiKey="")
+NPR = Configuration(apiKey="")
+Tumblr = Configuration(apiKey="", apiSecret="")
+NYT = Configuration(apiKey="")
 
 
